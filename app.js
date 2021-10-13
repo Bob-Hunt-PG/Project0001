e0be25d498f41fdeb3fa1e23d7b86176d598c4c5
Bob Hunt <85318042+Bob-Hunt@users.noreply.github.com>
2021-10-13 15:22:51 -0400

    Added app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// Company Information
+let coName = "Anywhere Products, LLC";
+let coStreet = "4275 Weber Hill Road";
+let coCity = "Trafalgar";
+let coState = "IN";
+let coZip = "46181";
+let coPhone = "3175043206";
+let coEmail = "anywherepdt@gmail.com";
+let coStatement = "Anywhere you live. Anywhere you work. Anywhere you play.";
+let valueProposition = "Anywhere Products, LLC provides quality products and services for land management of outdoor properties.";
+let coLogo = "./images/Anywhere-Products.jpg";
+let fullAddress = `${coStreet}, ${coCity}, ${coState} ${coZip}`;
+
+// Product Images
+// 2SGS-BlueSky
+let image1 = "./images/2SGS-BlueSky.jpg";
+// 2SGS-SolarFarm
+let image2 = "./images/2SGS-SolarFarm.jpg";
+// CrossLinerLite2
+let image3 = "./images/CossLinerLite2.jpg;"
+// dalokove_ovladani
+let image4= "./images/dalokove_ovlandani.jpg";
+
+
+let logoElement = '<img source=logo alt="Anywhere Products, LLC logo" >'
+document.getElementById('logo');
+
